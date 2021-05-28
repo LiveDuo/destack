62de9ecfd23b70587a27b734e93170e36eb5016e
JcdeA <jcde@jcde.xyz>
2021-05-28 23:10:08 +0900

    feat: switch to esbuild instead of babel; much faster && easier to add ts later

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
+import esbuild from 'rollup-plugin-esbuild'
 import resolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
 import css from "rollup-plugin-import-css";
 import svg from 'rollup-plugin-svg-import';
 import commonjs from '@rollup/plugin-commonjs';
@@ -22,10 +22,29 @@ const browserConfig = {
     svg({ stringify: false }),
     css(),
     resolve({ preferBuiltins: true }),
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'bundled'
-    })
+    esbuild({
+      // All options are optional
+      include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+      exclude: /node_modules/, // default
+      sourceMap: false, // default
+      minify: process.env.NODE_ENV === 'production',
+      target: 'es2017', // default, or 'es20XX', 'esnext'
+      jsxFactory: 'React.createElement',
+      jsxFragment: 'React.Fragment',
+      // Like @rollup/plugin-replace
+      define: {
+        __VERSION__: '"x.y.z"',
+      },
+      tsconfig: 'tsconfig.json', // default
+      // Add extra loaders
+      loaders: {
+        // Add .json files support
+        // require @rollup/plugin-commonjs
+        '.json': 'json',
+        // Enable JSX in .js files too
+        '.js': 'jsx',
+      },
+    }),
   ]
 }
 
@@ -38,9 +57,33 @@ const nodeConfig = {
   plugins: [
     nodePolyfills(),
     resolve(), 
-    commonjs(),
-    babel({babelHelpers: 'bundled'})
+    commonjs(),  
+      esbuild({
+        // All options are optional
+        include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+        exclude: /node_modules/, // default
+        sourceMap: false, // default
+        minify: process.env.NODE_ENV === 'production',
+        target: 'es2017', // default, or 'es20XX', 'esnext'
+        jsxFactory: 'React.createElement',
+        jsxFragment: 'React.Fragment',
+        // Like @rollup/plugin-replace
+        define: {
+          __VERSION__: '"x.y.z"',
+        },
+        tsconfig: 'tsconfig.json', // default
+        // Add extra loaders
+        loaders: {
+          // Add .json files support
+          // require @rollup/plugin-commonjs
+          '.json': 'json',
+          // Enable JSX in .js files too
+          '.js': 'jsx',
+        },
+      }),
+    
   ]
 }
+
 
 export default [browserConfig, nodeConfig]
