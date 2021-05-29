435b9c4977d253620c97dadc823ed353721836f1
JcdeA <jcde@jcde.xyz>
2021-05-29 22:18:38 +0900

    fix: use local typescript version

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  semi: false,
-  printWidth: 100,
-  trailingComma: 'all',
-  singleQuote: true,
-  tabWidth: 2,
-  useTabs: false,
+	semi: false,
+	printWidth: 100,
+	trailingComma: 'all',
+	singleQuote: true,
+	tabWidth: 2,
+	useTabs: true,
 }
