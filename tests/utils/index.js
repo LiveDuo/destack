5da15d393ad903ed8fcbccbf8c4921df8b177098
Andreas Tzionis <andy0n7@gmail.com>
2021-06-04 22:00:38 +0300

    added copyTemplateToTemp & copyTemplateFromTemp

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const os = require('os')
 const { exec } = require('child_process')
 
 const { Transform } = require('stream')
@@ -8,7 +9,7 @@ const debug = false
 
 const projectPath = 'dev/nextjs-project'
 
-const filepath = path.join(__dirname, '..', projectPath, 'data', 'default.json')
+const filepath = path.join(__dirname, '..', '..', projectPath, 'data', 'default.json')
 
 const deleteData = async () => {
   const hasData = await fs.promises
@@ -72,3 +73,15 @@ const killServer = async (port) => {
   }
 }
 exports.killServer = killServer
+
+const copyTemplateToTemp = async () => {
+  await fs.promises.copyFile(filepath, path.join(os.tmpdir(), 'default.json'))
+}
+exports.copyTemplateToTemp = copyTemplateToTemp
+
+const copyTemplateFromTemp = async () => {
+  const tempPath = path.join(os.tmpdir(), 'default.json')
+  await fs.promises.copyFile(tempPath, filepath)
+  await fs.promises.unlink(tempPath)
+}
+exports.copyTemplateFromTemp = copyTemplateFromTemp
