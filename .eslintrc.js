58e7403af6d4a8c256c00d94bb26633fbe78de33
JcdeA <jcde@jcde.xyz>
2021-06-08 11:48:25 +0900

    fix: edit config
    edit gitignore
    edit esilntrc(remove unneeded deps)

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,14 +21,12 @@ module.exports = {
         'plugin:@typescript-eslint/recommended', // TypeScript rules
         'plugin:react/recommended', // React rules
         'plugin:react-hooks/recommended', // React hooks rules
-        'plugin:jsx-a11y/recommended', // Accessibility rules
         'plugin:prettier/recommended', // Prettier recommended rules
       ],
       rules: {
         'react/prop-types': 'off', // We will use TypeScript's types for component props instead
         'react/display-name': 'off',
         'react/react-in-jsx-scope': 'off', // No need to import React with Next.js
-        'jsx-a11y/anchor-is-valid': 'off', // This rule is not compatible with how Next.js's <Link />
         '@typescript-eslint/no-unused-vars': ['error'],
         '@typescript-eslint/explicit-function-return-type': [
           // I suggest this setting for requiring return types on functions only where usefull
