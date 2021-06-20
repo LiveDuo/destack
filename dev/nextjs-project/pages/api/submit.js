8c3216fabdc638b504dad54b4747245dcfcaf426
Andreas Tzionis <andy0n7@gmail.com>
2021-06-20 20:28:15 +0300

    added form parsing to form submission api example

--- dev/nextjs-project/pages/api/submit.js
+++ dev/nextjs-project/pages/api/submit.js
@@ -1,4 +1,15 @@
-export default (_, res) => {
-  // res.status(200).json({ message: 'Form Submitted' })
-  res.status(400).json({ message: 'Something went wrong' })
+import formidable from 'formidable'
+
+const form = new formidable.IncomingForm()
+
+const formParse = (req) => new Promise((r, j) => form.parse(req, (e, f) => (!e ? r(f) : j(e))))
+
+export default (req, res) => {
+  try {
+    const fields = await formParse(req)
+    console.log(fields)
+    res.status(200).json({ message: 'Form Submitted' })
+  } catch (error) {
+    res.status(400).json({ message: 'Something went wrong' })
+  }
 }
