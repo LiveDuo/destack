d9f941810912b4292eb8edc040413add474fa73d
Andreas Tzionis <andy0n7@gmail.com>
2021-12-10 01:31:31 +0200

    moved component code to library

--- dev/react-project/src/components/App.js
+++ dev/react-project/src/components/App.js
@@ -1,34 +1,12 @@
 require('../styles/index.module.css')
 
-import { useEffect, useState } from 'react'
-
-import { ContentProvider } from 'destack'
-
-const isDev = process.env.NODE_ENV !== 'production'
+import { ContentProviderReact } from 'destack'
 
 const App = () => {
-  const [loaded, setLoaded] = useState(false)
-  const [data, setData] = useState(false)
-
-  useEffect(() => {
-    if (!isDev) {
-      fetch('/data/default.json')
-        .then((response) => response.json())
-        .then((_data) => {
-          setData(_data)
-          setLoaded(true)
-        })
-    } else {
-      setData({})
-      setLoaded(true)
-    }
-  }, [])
-
   return (
     <div style={{ height: '100%' }}>
-      {loaded && <ContentProvider html={data.html} css={data.css} />}
+      <ContentProviderReact />
     </div>
   )
 }
-
 export default App
---
+++ lib/destack/src/components/react.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+import { ContentProvider } from './index'
+
+const isDev = process.env.NODE_ENV !== 'production'
+
+const ContentProviderReact = () => {
+  const [loaded, setLoaded] = useState(false)
+  const [data, setData] = useState(false)
+
+  useEffect(() => {
+    if (!isDev) {
+      fetch('/data/default.json')
+        .then((response) => response.json())
+        .then((_data) => {
+          setData(_data)
+          setLoaded(true)
+        })
+    } else {
+      setData({})
+      setLoaded(true)
+    }
+  }, [])
+
+  return (
+    <div style={{ height: '100%' }}>
+      {loaded && <ContentProvider html={data.html} css={data.css} />}
+    </div>
+  )
+}
+
+export { ContentProviderReact }
