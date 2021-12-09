b84d3adab2bc2792f2726266947303e29c75b56f
Andreas Tzionis <andy0n7@gmail.com>
2021-12-09 13:34:21 +0200

    added local react server

---
+++ lib/destack/server/react.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+
+import { handleData } from './api/handle'
+
+const app = express()
+const port = 3000
+
+app.use(cors({ credentials: true, origin: true }))
+
+app.get('/ping', (_, res) => {
+  res.send('pong!')
+})
+
+app.get('/api/builder/handle', cors(), (req, res) => {
+  return handleData(req, res)
+})
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`)
+})
