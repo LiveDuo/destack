0b8ef668af9356a83579413c4d7d1db3c90e88d5
nikolas-con <nikos.a.constantinou@gmail.com>
2022-10-04 20:20:11 +0300

    added hyper ui

--- lib/destack/src/lib/blocks/index.ts
+++ lib/destack/src/lib/blocks/index.ts
@@ -1,8 +1,10 @@
 import { loadBasicBlocks } from './basic'
 // import {loadFormBlocks} from './form'
 import { loadTailwindBlocks } from './tailblocks'
+import { loadHyperUiBlocks } from './hyperui'
 
 export function loadBlocks(editor) {
   loadBasicBlocks(editor)
-  loadTailwindBlocks(editor)
+  // loadTailwindBlocks(editor)
+  loadHyperUiBlocks(editor)
 }
