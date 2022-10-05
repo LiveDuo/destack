0700c4f44308672507c092d973ad11f8170053b3
nikolas-con <nikos.a.constantinou@gmail.com>
2022-10-05 15:23:33 +0300

    added meraki ui light

--- lib/destack/src/lib/blocks/index.ts
+++ lib/destack/src/lib/blocks/index.ts
@@ -1,10 +1,12 @@
 import { loadBasicBlocks } from './basic'
 // import {loadFormBlocks} from './form'
-import { loadTailwindBlocks } from './tailblocks'
-import { loadHyperUiBlocks } from './hyperui'
+// import { loadTailwindBlocks } from './tailblocks'
+// import { loadHyperUiBlocks } from './hyperui'
+import { loadMerakiUiLigthBlocks } from './merakiui-light'
 
 export function loadBlocks(editor) {
   loadBasicBlocks(editor)
   // loadTailwindBlocks(editor)
-  loadHyperUiBlocks(editor)
+  // loadHyperUiBlocks(editor)
+  loadMerakiUiLigthBlocks(editor)
 }
