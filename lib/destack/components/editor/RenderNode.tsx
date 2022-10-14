ed8e3c1520e11e4beb92e0dbf53f26af87efa9be
Andreas Tzionis <andy0n7@gmail.com>
2022-10-14 05:24:47 +0300

    removed public folder

--- lib/destack/components/editor/RenderNode.tsx
+++ lib/destack/components/editor/RenderNode.tsx
@@ -4,9 +4,9 @@ import React, { useEffect, useRef, useCallback } from 'react'
 import ReactDOM from 'react-dom'
 import styled from 'styled-components'
 
-import ArrowUp from '../../public/icons/arrow-up.svg'
-import Delete from '../../public/icons/delete.svg'
-import Move from '../../public/icons/move.svg'
+import ArrowUp from '@material-ui/icons/ArrowUpward'
+import Delete from '@material-ui/icons/Delete'
+import Move from '@material-ui/icons/MoveToInbox'
 
 const IndicatorDiv = styled.div`
   height: 30px;
