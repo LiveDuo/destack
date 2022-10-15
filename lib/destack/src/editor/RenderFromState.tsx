2a00a0fad4822a507f7546958be3fb1bcab0d098
Andreas Tzionis <andy0n7@gmail.com>
2022-10-15 12:23:16 +0300

    added render from state

---
+++ lib/destack/src/editor/RenderFromState.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Banner1 from '../selectors/Banner1'
+
+import json from '../data/default'
+
+const RenderFromState = () => {
+  return <Banner1 {...json['1zTA0CrnpF'].props}></Banner1>
+}
+
+export default RenderFromState
