a9dac3670af8e91b9570688c6f487a66ac7a59dd
Andreas Tzionis <andy0n7@gmail.com>
2022-10-16 02:55:49 +0300

    removed paragraphs

--- lib/destack/src/selectors/Container/ContainerSettings.tsx
+++ lib/destack/src/selectors/Container/ContainerSettings.tsx
@@ -16,31 +16,7 @@ export const ContainerSettings = () => {
         <ToolbarItem propKey="width" type="text" label="Width" />
         <ToolbarItem propKey="height" type="text" label="Height" />
       </ToolbarSection>
-      <ToolbarSection
-        title="Colors"
-        props={['background', 'color']}
-        summary={({ background, color }: any) => {
-          return (
-            <div className="flex flex-row-reverse">
-              <div
-                style={{
-                  background: background && `rgba(${Object.values(background)})`,
-                }}
-                className="shadow-md flex-end w-6 h-6 text-center flex items-center rounded-full bg-black"
-              >
-                <p
-                  style={{
-                    color: color && `rgba(${Object.values(color)})`,
-                  }}
-                  className="text-white w-full text-center"
-                >
-                  T
-                </p>
-              </div>
-            </div>
-          )
-        }}
-      >
+      <ToolbarSection title="Colors" props={['background', 'color']}>
         <ToolbarItem full={true} propKey="background" type="bg" label="Background" />
         <ToolbarItem full={true} propKey="color" type="color" label="Text" />
       </ToolbarSection>
