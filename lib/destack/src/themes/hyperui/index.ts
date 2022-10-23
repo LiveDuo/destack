1fc5589dc0c12fd7ddcc00b019fa64f18e783872
Andreas Tzionis <andy0n7@gmail.com>
2022-10-23 16:36:09 +0300

    added component to hyper ui

--- lib/destack/src/themes/hyperui/index.ts
+++ lib/destack/src/themes/hyperui/index.ts
@@ -1,6 +1,7 @@
 import Banner1 from './Banner1/index'
 import Banner2 from './Banner2/index'
+import Banner3 from './Banner3/index'
 import Test from './Test/index'
 
-const components = { Banner1, Banner2, Test }
+const components = { Banner1, Banner2, Banner3, Test }
 export default components
