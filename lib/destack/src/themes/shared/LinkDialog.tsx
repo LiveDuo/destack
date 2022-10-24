115e64e48eab80a1aad9e20bf4a988ff20b5506c
Andreas Tzionis <andy0n7@gmail.com>
2022-10-24 23:28:14 +0300

    worked on link dialog

--- lib/destack/src/themes/shared/LinkDialog.tsx
+++ lib/destack/src/themes/shared/LinkDialog.tsx
@@ -3,10 +3,23 @@ import React, { useState } from 'react'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 
+import Select from '../../components/Select'
+
+import { ChevronDownIcon } from '@heroicons/react/24/outline'
+
 import cx from 'classnames'
 
+const options = ['Url', 'Email', 'Submit']
+
 const Dialog = ({ currentUrl, open, setOpen, actions }) => {
-  const [url, setUrl] = useState<string | null>(currentUrl)
+  const [url, setUrl] = useState(currentUrl)
+
+  const [openSelect, setOpenSelect] = useState(false)
+  const [selected, setSelected] = useState(0)
+  const onChange = (e) => {
+    setSelected(options.indexOf(e))
+  }
+
   return (
     <DialogPrimitive.Root open={open} onOpenChange={setOpen}>
       <DialogPrimitive.Portal>
@@ -25,14 +38,54 @@ const Dialog = ({ currentUrl, open, setOpen, actions }) => {
             <DialogPrimitive.Description className="mt-2 text-sm font-normal text-gray-700 dark:text-gray-400">
               <div className="mt-4 mb-4">
                 <div>
-                  <div className="flex justify-center mb-4">
-                    <input
-                      type="text"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                      placeholder="Eg. https://github.com/LiveDuo/destack"
-                      defaultValue={url as string}
-                      onChange={(e) => setUrl(e.target.value)}
-                    />
+                  <div
+                    className="flex rounded py-2 px-4 transition cursor-pointer items-center ml-2 mb-4"
+                    onClick={() => setOpenSelect(true)}
+                  >
+                    {options[selected]} <ChevronDownIcon className="h-4 w-4 ml-2" />
+                  </div>
+                  <Select
+                    defaultValue={options[selected]}
+                    values={options}
+                    open={openSelect}
+                    setOpen={setOpenSelect}
+                    onChange={onChange}
+                  />
+
+                  <div>
+                    {/* Url */}
+                    {selected === 0 && (
+                      <div className="flex justify-center mb-4 flex-col">
+                        <input
+                          type="text"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-4"
+                          placeholder="Eg. https://github.com/LiveDuo/destack"
+                          defaultValue={url as string}
+                          onChange={(e) => setUrl(e.target.value)}
+                        />
+                        <div className="flex items-center ml-4">
+                          <p>Open in new tab</p>
+                          <input
+                            type="checkbox"
+                            className="ml-4 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
+                          />
+                        </div>
+                      </div>
+                    )}
+                    {/* Email */}
+                    {selected === 1 && (
+                      <div className="flex justify-center mb-4">
+                        <input
+                          type="text"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+                          placeholder="Eg. matt@mullenweg.com"
+                          defaultValue={url as string}
+                          onChange={(e) => setUrl(e.target.value)}
+                        />
+                      </div>
+                    )}
+                    {/* Submit */}
+                    {selected === 2 && <div className="flex justify-center mb-4">{/* ... */}</div>}
                   </div>
                 </div>
               </div>
