f0c3081fef1e2aebd372562a0b9be9a1812ca455
Andreas Tzionis <andy0n7@gmail.com>
2022-10-25 03:27:34 +0300

    move theme provider to top

--- lib/destack/src/index.tsx
+++ lib/destack/src/index.tsx
@@ -2,22 +2,28 @@ import React from 'react'
 
 import RenderFromEditor from './editor/FrameFromEditor'
 
+import { ThemeProvider } from './store'
+
 import './styles/app.css'
 
 const ContentProvider = ({ data }) => {
   return (
-    <div className="h-full h-screen">
-      <RenderFromEditor data={data} standaloneServer={false} />
-    </div>
+    <ThemeProvider>
+      <div className="h-full h-screen">
+        <RenderFromEditor data={data} standaloneServer={false} />
+      </div>
+    </ThemeProvider>
   )
 }
 export { ContentProvider }
 
 const ContentProviderReact = () => {
   return (
-    <div className="h-full h-screen">
-      <RenderFromEditor data={null} standaloneServer={true} />
-    </div>
+    <ThemeProvider>
+      <div className="h-full h-screen">
+        <RenderFromEditor data={null} standaloneServer={true} />
+      </div>
+    </ThemeProvider>
   )
 }
 export { ContentProviderReact }
--- lib/destack/src/viewport/index.tsx
+++ lib/destack/src/viewport/index.tsx
@@ -5,8 +5,6 @@ import { useEditor } from '@craftjs/core'
 import { Header } from './Header'
 import { Sidebar as LeftSidebar } from './LeftSidebar'
 
-import { ThemeProvider } from '../store'
-
 export const Viewport: React.FC = ({ children }) => {
   const { connectors, actions } = useEditor((state) => ({ enabled: state.options.enabled }))
 
@@ -17,25 +15,23 @@ export const Viewport: React.FC = ({ children }) => {
   }, [actions.setOptions])
 
   return (
-    <ThemeProvider>
-      <div className="viewport">
-        <div className="flex h-full overflow-hidden flex-row w-full fixed">
-          <LeftSidebar />
-          <div className="page-container flex flex-1 h-full flex-col">
-            <Header />
-            <div
-              className="craftjs-renderer flex-1 h-full w-full transition pb-8 overflow-auto"
-              ref={(ref) => connectors.select(connectors.hover(ref as HTMLElement, ''), '')}
-            >
-              <div className="relative flex-col flex items-center pt-8">{children}</div>
-              <div className={'flex items-center justify-center w-full pt-6 text-xs text-gray-400'}>
-                <a href="https://www.netlify.com">This site is powered by Netlify</a>
-              </div>
+    <div className="viewport">
+      <div className="flex h-full overflow-hidden flex-row w-full fixed">
+        <LeftSidebar />
+        <div className="page-container flex flex-1 h-full flex-col">
+          <Header />
+          <div
+            className="craftjs-renderer flex-1 h-full w-full transition pb-8 overflow-auto"
+            ref={(ref) => connectors.select(connectors.hover(ref as HTMLElement, ''), '')}
+          >
+            <div className="relative flex-col flex items-center pt-8">{children}</div>
+            <div className={'flex items-center justify-center w-full pt-6 text-xs text-gray-400'}>
+              <a href="https://www.netlify.com">This site is powered by Netlify</a>
             </div>
           </div>
-          {/* <RightSidebar /> */}
         </div>
+        {/* <RightSidebar /> */}
       </div>
-    </ThemeProvider>
+    </div>
   )
 }
