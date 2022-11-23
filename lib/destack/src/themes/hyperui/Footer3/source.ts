6ed46d54f4904d93ba40cc1b49cb82bfffb5e9a5
nikolas-con <nikos.a.constantinou@gmail.com>
2022-11-23 18:16:36 +0200

    fixed navigation icons , footer icons and footer buttons

--- lib/destack/src/themes/hyperui/Footer3/source.ts
+++ lib/destack/src/themes/hyperui/Footer3/source.ts
@@ -26,16 +26,16 @@ const source = `
       </nav>
 
       <div class="mt-6 flex justify-center gap-6 sm:mt-0 sm:justify-end">
-        <a
-          class="p-2 hover:opacity-75"
-          href=""
-          target="_blank"
+      <a
+          href="/"
           rel="noreferrer"
+          target="_blank"
+          class="text-white transition hover:opacity-75"
         >
-          <span class="sr-only"> Facebook </span>
+          <span class="sr-only">Facebook</span>
 
           <svg
-            class="h-4 w-4"
+            class="w-6 h-6"
             fill="currentColor"
             viewBox="0 0 24 24"
             aria-hidden="true"
@@ -49,15 +49,15 @@ const source = `
         </a>
 
         <a
-          class="rounded-full border border-white/25 p-2 hover:opacity-75"
-          href=""
-          target="_blank"
-          rel="noreferrer"
+        href="/"
+        rel="noreferrer"
+        target="_blank"
+        class="text-white transition hover:opacity-75"
         >
-          <span class="sr-only"> Instagram </span>
+          <span class="sr-only">Instagram</span>
 
           <svg
-            class="h-4 w-4"
+            class="w-6 h-6"
             fill="currentColor"
             viewBox="0 0 24 24"
             aria-hidden="true"
@@ -71,24 +71,25 @@ const source = `
         </a>
 
         <a
-          class="rounded-full border border-white/25 p-2 hover:opacity-75"
-          href=""
-          target="_blank"
-          rel="noreferrer"
-        >
-          <span class="sr-only"> Twitter </span>
-
-          <svg
-            class="h-4 w-4"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
+            href="/"
+            rel="noreferrer"
+            target="_blank"
+            class="text-white transition hover:opacity-75"
           >
-            <path
-              d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
-            />
-          </svg>
+            <span class="sr-only">Twitter</span>
+
+            <svg
+              class="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+            >
+              <path
+                d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
+              />
+            </svg>
         </a>
+        
       </div>
     </div>
   </div>
