0ec5d4b367bdfb3751c323798464081b372507bd
Andreas Tzionis <andy0n7@gmail.com>
2022-11-27 19:47:28 +0200

    simplified container

--- lib/destack/src/themes/shared/Container.tsx
+++ lib/destack/src/themes/shared/Container.tsx
@@ -2,21 +2,18 @@ import React from 'react'
 
 import { useNode } from '@craftjs/core'
 
-import { Resizable } from 're-resizable'
-
 export const Container = ({ children }) => {
   const { connectors, node } = useNode((node) => ({ node }))
   const { width, height } = node.data.props
 
   return (
-    <Resizable
-      enable={{}}
-      ref={(ref) => connectors.connect(ref?.resizable as HTMLElement)}
-      size={{ width: width ?? '100%', height: height ?? '100%' }}
+    <div
+      ref={(ref) => connectors.connect(ref as HTMLElement)}
+      style={{ width: width ?? '100%', height: height ?? '100%' }}
       className="bg-white"
     >
       {children}
-    </Resizable>
+    </div>
   )
 }
 
