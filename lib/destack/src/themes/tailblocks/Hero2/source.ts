b8b606495ba1a4d1400840f4a10b2aa9385994ea
Andreas Tzionis <andy0n7@gmail.com>
2022-12-09 18:11:55 +0200

    fix hero 2 in tailblocks

--- lib/destack/src/themes/tailblocks/Hero2/source.ts
+++ lib/destack/src/themes/tailblocks/Hero2/source.ts
@@ -1,7 +1,7 @@
 const source = `
 <section class="text-gray-600 body-font">
   <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
+    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 ml-auto mr-auto object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
     <div class="text-center lg:w-2/3 w-full">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Microdosing synth tattooed vexillologist</h1>
       <p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
