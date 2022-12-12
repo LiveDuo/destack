83f0e2e5595bea0128624117437391b2b5052eb5
Andreas Tzionis <andy0n7@gmail.com>
2022-12-12 20:03:38 +0200

    fixed more css issues

--- lib/destack/src/themes/hyperui/Form3/source.ts
+++ lib/destack/src/themes/hyperui/Form3/source.ts
@@ -103,7 +103,7 @@ const source = `
     <img
       alt="Welcome"
       src="https://images.unsplash.com/photo-1630450202872-e0829c9d6172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-      class="absolute inset-0 h-full w-full object-cover"
+      class="inset-0 h-full w-full object-cover"
     />
   </div>
 </section>
--- lib/destack/src/themes/tailblocks/Gallery2/source.ts
+++ lib/destack/src/themes/tailblocks/Gallery2/source.ts
@@ -3,7 +3,7 @@ const source = `
   <div class="container px-5 py-24 mx-auto flex flex-wrap">
     <div class="lg:w-2/3 mx-auto">
       <div class="flex flex-wrap w-full bg-gray-100 py-32 px-10 relative mb-4">
-        <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://dummyimage.com/820x340">
+        <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 inset-0" src="https://dummyimage.com/820x340">
         <div class="text-center relative z-10 w-full">
           <h2 class="text-2xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
           <p class="leading-relaxed">Skateboard +1 mustache fixie paleo lumbersexual.</p>
@@ -17,7 +17,7 @@ const source = `
       <div class="flex flex-wrap -mx-2">
         <div class="px-2 w-1/2">
           <div class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
-            <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://dummyimage.com/542x460">
+            <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 inset-0" src="https://dummyimage.com/542x460">
             <div class="text-center relative z-10 w-full">
               <h2 class="text-xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
               <p class="leading-relaxed">Skateboard +1 mustache fixie paleo lumbersexual.</p>
@@ -31,7 +31,7 @@ const source = `
         </div>
         <div class="px-2 w-1/2">
           <div class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
-            <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://dummyimage.com/542x420">
+            <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 inset-0" src="https://dummyimage.com/542x420">
             <div class="text-center relative z-10 w-full">
               <h2 class="text-xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
               <p class="leading-relaxed">Skateboard +1 mustache fixie paleo lumbersexual.</p>
