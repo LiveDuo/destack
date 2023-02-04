e2c56cda6429d8d9a1ef93cff547b73920759cca
Andreas Tzionis <andy0n7@gmail.com>
2023-02-04 13:28:37 +0200

    moved lib folder

--- dev/nextjs-project/pages/_app.js
+++ dev/nextjs-project/pages/_app.js
@@ -1,4 +1,4 @@
-require('../../../lib/destack/build/browser/index.css')
+require('../../../lib/build/browser/index.css')
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
