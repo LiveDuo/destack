a8e906615afda142feaabe81fe8636b1a6d7ca6a
Andreas Tzionis <andy0n7@gmail.com>
2023-02-04 19:59:45 +0200

    fixed bug with tailwind classes

--- lib/client/craft/utils/html.ts
+++ lib/client/craft/utils/html.ts
@@ -3,7 +3,7 @@ const cleanHTMLElement = (root) => {
     childNodes: root.childNodes.map(cleanHTMLElement),
     attrs: root.attrs,
     tagName: root.tagName,
-    classNames: root.classNames,
+    classNames: root?.classNames.join(' '),
     nodeType: root.nodeType,
     innerText: root.innerText,
     constructor: root.constructor.name,
