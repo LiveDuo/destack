b35f167d92cf41764a915a7029d26040915fc612
Andreas Tzionis <andy0n7@gmail.com>
2023-03-02 14:16:51 +0200

    added analyze command

--- dev/nextjs-project/next.config.js
+++ dev/nextjs-project/next.config.js
@@ -1,3 +1,4 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({ reactStrictMode: true })
