f134b546d1b0f367abc5043b2231025b418f7b35
Andreas Tzionis <andy0n7@gmail.com>
2023-06-13 12:26:53 +0300

    added cards

---
+++ lib/themes/preline/Cards1/index.tsx
@@ -0,0 +1,5 @@
+const Component = {
+  displayName: 'Cards 1',
+  category: 'Cards',
+}
+export default Component
---
+++ lib/themes/preline/Cards4/index.tsx
@@ -0,0 +1,5 @@
+const Component = {
+  displayName: 'Cards 3',
+  category: 'Cards',
+}
+export default Component
