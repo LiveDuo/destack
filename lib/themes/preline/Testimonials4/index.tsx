cff22b492df3325ab4e57774c1c2f40909dace7d
Andreas Tzionis <andy0n7@gmail.com>
2023-06-13 12:27:44 +0300

    added testimonials

---
+++ lib/themes/preline/Testimonials4/index.tsx
@@ -0,0 +1,5 @@
+const Component = {
+  displayName: 'Testimonials 4',
+  category: 'Testimonials',
+}
+export default Component
---
+++ lib/themes/preline/Testimonials7/index.tsx
@@ -0,0 +1,5 @@
+const Component = {
+  displayName: 'Testimonials 7',
+  category: 'Testimonials',
+}
+export default Component
