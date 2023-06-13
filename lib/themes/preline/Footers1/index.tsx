93353962a58d7b8834b15e553ee76fc8d2e653bf
Andreas Tzionis <andy0n7@gmail.com>
2023-06-13 13:22:26 +0300

    added footers and more forms

---
+++ lib/themes/preline/Footers1/index.tsx
@@ -0,0 +1,5 @@
+const Component = {
+  displayName: 'Footer 1',
+  category: 'Footers',
+}
+export default Component
---
+++ lib/themes/preline/Forms4/index.tsx
@@ -0,0 +1,5 @@
+const Component = {
+  displayName: 'Form 4',
+  category: 'Forms',
+}
+export default Component
