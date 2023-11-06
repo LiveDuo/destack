fe43d0d5caaf2d3c978f91821f7b827b9b2eb612
Andreas Tzionis <andy0n7@gmail.com>
2023-11-07 01:27:39 +0200

    worked on upload test

--- e2e/dev/dev.spec.ts
+++ e2e/dev/dev.spec.ts
@@ -9,7 +9,7 @@ test('should drap and drop a component', async ({ page }) => {
   await page.goto('/')
 
   // open category
-  await page.click('text=Βanner')
+  await page.click('text=BANNER')
 
   // add component
   const imagePath = '/api/builder/handle?type=asset&path=/themes/hyperui/Banner1/preview.png'
@@ -22,8 +22,7 @@ test('should drap and drop a component', async ({ page }) => {
   await page.mouse.move(deleteElement?.x as number, deleteElement?.y as number)
 })
 
-// TODO fix
-test.skip('should add an image to renderer', async ({ page }) => {
+test('should add an image to renderer', async ({ page }) => {
   await page.goto('/')
 
   // open category
@@ -35,17 +34,14 @@ test.skip('should add an image to renderer', async ({ page }) => {
 
   // open image dialog
   await page.hover('text= Lorem')
-  const imageElement = await page.locator('#editor img').first().boundingBox() // TODO fix
-  await page.mouse.move(imageElement?.x as number, imageElement?.y as number)
+  const imageElement = await page.getByRole('img', { name: 'Student' }).first()
+  await imageElement.click()
 
   // click replace image
   await page.click('text=Replace')
   await page.setInputFiles("input[type='file']", ['e2e/dev/pattern.jpg'])
   await page.click('text=Upload')
   await page.click('text=Save')
-
-  // await page.waitForTimeout(5000)
-  // return
 
   // check image uploaded
   await expect(page.locator(`img[src='/uploaded/pattern.jpg']`)).toHaveCount(1)
