215e90bc7d8cae1a688c95097842a01fca0ea10a
Andreas Tzionis <andy0n7@gmail.com>
2023-11-07 11:37:45 +0200

    enabled react build test

--- e2e/build-react/build.spec.ts
+++ e2e/build-react/build.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-// TODO fix
-test.skip('should show the production version', async ({ page }) => {
+test('should show the production version', async ({ page }) => {
   await page.goto('/')
   await expect(page).toHaveURL('/')
   await expect(page.locator('#page')).toBeVisible()
