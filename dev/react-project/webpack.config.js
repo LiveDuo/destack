2604ae48a0f2fd4dadcfe224c249dd34144ab75b
Andreas Tzionis <andy0n7@gmail.com>
2023-11-07 11:26:30 +0200

    fixed react dev project build

--- dev/react-project/webpack.config.js
+++ dev/react-project/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV ?? 'development',
   entry: '/src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'build'),
