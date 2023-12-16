0132422fe79180352c16bfa47f2b9d625b26832e
Andreas Tzionis <andy0n7@gmail.com>
2023-12-16 21:44:32 +0200

    fixed grapes page

--- dev/nextjs-project/pages/grapes.js
+++ dev/nextjs-project/pages/grapes.js
@@ -1,12 +1,12 @@
 import '../../../lib/node_modules/grapesjs/dist/css/grapes.min.css'
 
 export { getStaticProps } from 'destack/build/server'
-import { ContentProviderGrapes } from 'destack'
+import { ContentProvider } from 'destack/build/browser/grapes'
 
 const Index = () => {
   return (
     <div style={{ height: '100%' }}>
-      <ContentProviderGrapes />
+      <ContentProvider />
     </div>
   )
 }
