ae136d87bc36fb79192f3dba939b18d3f3f4120a
Andreas Tzionis <andy0n7@gmail.com>
2024-03-30 01:41:25 +0200

    added page2 to example project

---
+++ dev/nextjs-project/pages/page2.js
@@ -0,0 +1,11 @@
+export { getStaticProps } from 'destack/build/server'
+import { ContentProvider } from 'destack'
+
+const Index = (props) => {
+  return (
+    <div style={{ height: '100%' }}>
+      <ContentProvider {...props} />
+    </div>
+  )
+}
+export default Index
